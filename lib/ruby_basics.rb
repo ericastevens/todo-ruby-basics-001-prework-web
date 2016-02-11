657a26c1514ed7079186be0a73a2ef680eb91101
ericastevens <estevens@nyc.yearup.org>
2016-02-11 23:29:59 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,38 @@
 def division(num1, num2)
- 
+  num1= 42
+  num2= 7
+  quotient = num1/num2
+  puts quotient
+  return quotient
 end
 
 def assign_variable(value)
-  
+  value= "Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  phrase= "I'm right and you are wrong!"
+  return phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting= "Hi there, "
+  name= "Bobby!"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "beginner"
+  "intermediate"
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  puts topping
+  return topping
 end
+
+pizza_party
